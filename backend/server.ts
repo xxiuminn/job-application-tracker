8a9c545d81b6b368d6f8ea22b33f4b9ee4c34545
xxiuminn <xiumin1997@gmail.com>
2024-08-13 15:27:45 +0800

    add typescript dependencies

---
+++ backend/server.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 300,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app: Express = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.port || 8000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
