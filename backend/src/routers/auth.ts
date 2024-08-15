b42f89aef571d2d72471ae2bdd7b21ccc3c4834f
xxiuminn <xiumin1997@gmail.com>
2024-08-15 22:42:16 +0800

    login register routers

--- backend/src/routers/auth.ts
+++ backend/src/routers/auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import register from "../controllers/auth";
+import { register, login } from "../controllers/auth";
 
 router.put("/register", register);
+router.post("/login", login);
 
 export default router;
