f22e21c4e2b20d29f91456374ea2ca3aac1c9391
xxiuminn <xiumin1997@gmail.com>
2024-08-16 22:56:35 +0800

    Update list.ts

--- backend/src/routers/list.ts
+++ backend/src/routers/list.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get("/all", list.getAllList);
 router.put("/create", list.createList);
 router.delete("/delete", list.delList);
+router.patch("/update", list.patchList);
 
 export default router;
