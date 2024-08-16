cfec7b2490a8c24693246e7a8fa722f2815892f5
xxiuminn <xiumin1997@gmail.com>
2024-08-16 22:56:26 +0800

    Create job.d.ts

---
+++ backend/src/interfaces/job.d.ts
@@ -0,0 +1,11 @@
+interface GetJobs {
+  title: string;
+  description: string;
+  url: string;
+  salary: string;
+  location: string;
+  attachment: string[];
+  listId: number;
+}
+
+export default GetJobs;
