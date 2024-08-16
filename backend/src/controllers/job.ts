4a6ed140cc5feed7c26d840152838f3f5aa3db78
xxiuminn <xiumin1997@gmail.com>
2024-08-16 22:56:19 +0800

    Update job.ts

---
+++ backend/src/controllers/job.ts
@@ -0,0 +1,115 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+const prisma = new PrismaClient();
+import GetJobs from "../interfaces/job";
+const getAllJobs = async (req: Request, res: Response) => {
+  const jobs = await prisma.job.findMany({
+    where: {
+      list: {
+        userId: req.body.user_id,
+      },
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      url: true,
+      salary: true,
+      location: true,
+      attachment: true,
+      list: {
+        select: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  res.json(jobs);
+  console.log(jobs);
+};
+
+const createJob = async (req: Request, res: Response) => {
+  try {
+    const {
+      title,
+      description,
+      url,
+      salary,
+      location,
+      attachment,
+      listId,
+    }: GetJobs = req.body;
+    await prisma.job.create({
+      data: {
+        title,
+        description,
+        url,
+        salary,
+        location,
+        attachment,
+        list: { connect: { id: listId } },
+      },
+    });
+    res.json("done");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      res.status(400).json({ status: "error", msg: "error creating job" });
+    } else
+      res
+        .status(500)
+        .json({ status: "error", msg: "unknown error creating job" });
+  }
+};
+
+const delJob = async (req: Request, res: Response) => {
+  try {
+    await prisma.job.delete({ where: { id: req.body.id } });
+    res.json({ status: "ok", msg: "job deleted" });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      res.status(400).json({ status: "error", msg: "error deleting job" });
+    } else
+      res
+        .status(500)
+        .json({ status: "error", msg: "unknown error deleting job" });
+  }
+};
+
+const patchJob = async (req: Request, res: Response) => {
+  const { id, title, description, url, salary, location, attachment, listId } =
+    req.body;
+  try {
+    await prisma.job.update({
+      where: { id },
+      data: {
+        title,
+        description,
+        url,
+        salary,
+        location,
+        attachment,
+        listId,
+      },
+    });
+    res.json({ status: "ok", msg: "job updated" });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      res.status(400).json({ status: "error", msg: "error updating job" });
+    } else
+      res
+        .status(500)
+        .json({ status: "error", msg: "unknown error updating job" });
+  }
+};
+
+export default { getAllJobs, createJob, delJob, patchJob };
