a41bde2932d4ac5a37b00a3783c0b485faaf8123
xxiuminn <xiumin1997@gmail.com>
2024-08-17 22:20:47 +0800

    middleware for zod

---
+++ backend/src/validators/checkErrors.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject, ZodError } from "zod";
+
+const checkErrors =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res.status(400).json({ status: "error", msg: errorMessages });
+      } else {
+        res
+          .status(500)
+          .json({ status: "error", msg: "unknown internal server error" });
+      }
+    }
+  };
+
+export default checkErrors;
