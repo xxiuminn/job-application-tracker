d96d4d4863aa57d5541f0341234ddfc2521862df
xxiuminn <xiumin1997@gmail.com>
2024-08-17 22:22:54 +0800

    update routers with input validators

--- backend/src/routers/auth.ts
+++ backend/src/routers/auth.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import auth from "../controllers/auth";
+import checkErrors from "../validators/checkErrors";
+import { RegistrationSchema, LoginSchema } from "../validators/auth";
 const router = express.Router();
 
-router.put("/register", auth.register);
-router.post("/login", auth.login);
+router.put("/register", checkErrors(RegistrationSchema), auth.register);
+router.post("/login", checkErrors(LoginSchema), auth.login);
 
 export default router;
