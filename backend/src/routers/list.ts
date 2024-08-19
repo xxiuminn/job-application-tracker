c598bc9fa6070e3e51325afb485322ef0fb63363
xxiuminn <xiumin1997@gmail.com>
2024-08-19 14:01:46 +0800

    Update list.ts

--- backend/src/routers/list.ts
+++ backend/src/routers/list.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import list from "../controllers/list";
+import checkErrors from "../validators/checkErrors";
+import {
+  GetAllListSchema,
+  CreateListSchema,
+  DelListSchema,
+  PatchListSchema,
+} from "../validators/list";
 const router = express.Router();
 
-router.get("/all", list.getAllList);
-router.put("/create", list.createList);
-router.delete("/delete", list.delList);
-router.patch("/update", list.patchList);
+router.get("/all", checkErrors(GetAllListSchema), list.getAllList);
+router.put("/create", checkErrors(CreateListSchema), list.createList);
+router.delete("/delete", checkErrors(DelListSchema), list.delList);
+router.patch("/update", checkErrors(PatchListSchema), list.patchList);
 
 export default router;
