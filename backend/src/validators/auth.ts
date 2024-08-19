be36c92b75a9b8a17fab701eb858fe9522038814
xxiuminn <xiumin1997@gmail.com>
2024-08-19 14:01:43 +0800

    Update auth.ts

--- backend/src/validators/auth.ts
+++ backend/src/validators/auth.ts
@@ -2,18 +2,37 @@ import { z } from "zod";
 
 const RegistrationSchema = z.object({
   name: z
-    .string({ message: "name must be a string" })
-    .nonempty({ message: "can't be empty" }),
-  email: z.string().email().nonempty({ message: "can't be empty" }),
+    .string({
+      required_error: "name is required",
+      invalid_type_error: "name must be a string",
+    })
+    .min(1, { message: "name field can't be empty" }),
+  email: z
+    .string({
+      required_error: "email is required",
+      invalid_type_error: "please check email",
+    })
+    .email()
+    .min(1, { message: "email field can't be empty" }),
   password: z
     .string()
-    .min(8, { message: "password must be more than 8 characters long" })
-    .nonempty({ message: "can't be empty" }),
+    .min(8, { message: "password must be more than 8 characters long" }),
 });
 
 const LoginSchema = z.object({
-  email: z.string().email().nonempty({ message: "can't be empty" }),
-  password: z.string().nonempty().min(8, { message: "must be more than 8" }),
+  email: z
+    .string({
+      required_error: "email is required",
+      invalid_type_error: "please check email",
+    })
+    .email()
+    .min(1, { message: "email field can't be empty" }),
+  password: z
+    .string({
+      required_error: "pw is required",
+      invalid_type_error: "pw must be a string",
+    })
+    .min(8, { message: "pw must be more than 8 characters" }),
 });
 
 export { RegistrationSchema, LoginSchema };
