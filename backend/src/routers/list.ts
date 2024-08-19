eaf539330bc1c3a88d5dde90c9e4848a38db1f28
xxiuminn <xiumin1997@gmail.com>
2024-08-19 20:22:38 +0800

    add auth middleware to routers

--- backend/src/routers/list.ts
+++ backend/src/routers/list.ts
@@ -2,16 +2,16 @@ import express from "express";
 import list from "../controllers/list";
 import checkErrors from "../validators/checkErrors";
 import {
-  GetAllListSchema,
   CreateListSchema,
   DelListSchema,
   PatchListSchema,
 } from "../validators/list";
+import auth from "../middleware/auth";
 const router = express.Router();
 
-router.get("/all", checkErrors(GetAllListSchema), list.getAllList);
-router.put("/create", checkErrors(CreateListSchema), list.createList);
-router.delete("/delete", checkErrors(DelListSchema), list.delList);
-router.patch("/update", checkErrors(PatchListSchema), list.patchList);
+router.get("/all", auth, list.getAllList);
+router.put("/create", auth, checkErrors(CreateListSchema), list.createList);
+router.delete("/delete", auth, checkErrors(DelListSchema), list.delList);
+router.patch("/update", auth, checkErrors(PatchListSchema), list.patchList);
 
 export default router;
