84e27699959bbc1a94d1eb30b5274453f5ffe513
xxiuminn <xiumin1997@gmail.com>
2024-08-19 14:01:36 +0800

    Create job.ts

---
+++ backend/src/validators/job.ts
@@ -0,0 +1,70 @@
+import { z } from "zod";
+
+const GetAllJobsSchema = z.object({
+  userId: z
+    .string({
+      required_error: "user id is required",
+      invalid_type_error: "user id must be a string",
+    })
+    .min(1, { message: "user id field cannot be empty" }),
+});
+
+const CreateJobSchema = z.object({
+  title: z
+    .string({
+      required_error: "title is required",
+      invalid_type_error: "title must be a string",
+    })
+    .min(1, { message: "title cannot be empty" }),
+  description: z.string().optional(),
+  url: z.string().optional(),
+  salary: z.string().optional(),
+  location: z.string().optional(),
+  attachment: z.string().array().optional(),
+  list_id: z
+    .number({
+      required_error: "user id is required",
+      invalid_type_error: "user id must be a string",
+    })
+    .nonnegative()
+    .min(1, { message: "list id field cannot be empty" }),
+});
+
+const DelJobSchema = z.object({
+  id: z
+    .number({
+      required_error: "id is required",
+      invalid_type_error: "id must be number",
+    })
+    .min(1, { message: "id field cannot be empty" }),
+});
+
+const PatchJobSchema = z.object({
+  id: z
+    .number({
+      required_error: "job id is required",
+      invalid_type_error: "job id must be a number",
+    })
+    .nonnegative()
+    .min(1, { message: "job id field cannot be empty" }),
+  title: z
+    .string({
+      required_error: "title is required",
+      invalid_type_error: "title must be a string",
+    })
+    .min(1, { message: "title cannot be empty" }),
+  description: z.string().optional(),
+  url: z.string().optional(),
+  salary: z.string().optional(),
+  location: z.string().optional(),
+  attachment: z.string().array().optional(),
+  list_id: z
+    .number({
+      required_error: "user id is required",
+      invalid_type_error: "user id must be a string",
+    })
+    .nonnegative()
+    .min(1, { message: "list id field cannot be empty" }),
+});
+
+export { GetAllJobsSchema, CreateJobSchema, DelJobSchema, PatchJobSchema };
