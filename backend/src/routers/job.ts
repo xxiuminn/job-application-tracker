91889214d6216015205ce5cde7f5ab77ca8a81db
xxiuminn <xiumin1997@gmail.com>
2024-08-19 14:01:49 +0800

    Update job.ts

--- backend/src/routers/job.ts
+++ backend/src/routers/job.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import job from "../controllers/job";
+import {
+  GetAllJobsSchema,
+  CreateJobSchema,
+  DelJobSchema,
+  PatchJobSchema,
+} from "../validators/job";
+import checkErrors from "../validators/checkErrors";
 const router = express.Router();
 
-router.get("/all", job.getAllJobs);
-router.put("/create", job.createJob);
-router.delete("/delete", job.delJob);
-router.patch("/update", job.patchJob);
+router.get("/all", checkErrors(GetAllJobsSchema), job.getAllJobs);
+router.put("/create", checkErrors(CreateJobSchema), job.createJob);
+router.delete("/delete", checkErrors(DelJobSchema), job.delJob);
+router.patch("/update", checkErrors(PatchJobSchema), job.patchJob);
 
 export default router;
