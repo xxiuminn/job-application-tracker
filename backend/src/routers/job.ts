5c742bd77d4e25eb3c79f70eec49ce6f8bc3e0f2
xxiuminn <xiumin1997@gmail.com>
2024-08-19 20:22:29 +0800

    add auth middleware to routers

--- backend/src/routers/job.ts
+++ backend/src/routers/job.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import job from "../controllers/job";
 import {
-  GetAllJobsSchema,
   CreateJobSchema,
   DelJobSchema,
   PatchJobSchema,
 } from "../validators/job";
 import checkErrors from "../validators/checkErrors";
+import auth from "../middleware/auth";
 const router = express.Router();
 
-router.get("/all", checkErrors(GetAllJobsSchema), job.getAllJobs);
-router.put("/create", checkErrors(CreateJobSchema), job.createJob);
-router.delete("/delete", checkErrors(DelJobSchema), job.delJob);
-router.patch("/update", checkErrors(PatchJobSchema), job.patchJob);
+router.get("/all", auth, job.getAllJobs);
+router.put("/create", auth, checkErrors(CreateJobSchema), job.createJob);
+router.delete("/delete", auth, checkErrors(DelJobSchema), job.delJob);
+router.patch("/update", auth, checkErrors(PatchJobSchema), job.patchJob);
 
 export default router;
