902293e37cc5b7c9ce76bdbd9118e13765a4fc45
xxiuminn <xiumin1997@gmail.com>
2024-08-20 22:05:32 +0800

    add queryclientprovider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import Board from "./components/Board";
+// import Board from "./components/Board";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./components/Home";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Board />
+      <QueryClientProvider client={queryClient}>
+        <Home />
+        {/* <Board /> */}
+      </QueryClientProvider>
     </>
   );
 }
