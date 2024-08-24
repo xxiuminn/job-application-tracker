a0dfd66b3865c8e4e03a03ceb40e20fcaff90cb4
xxiuminn <xiumin1997@gmail.com>
2024-08-24 17:02:46 +0800

    Update AddJobModal.tsx

--- frontend/src/components/AddJobModal.tsx
+++ frontend/src/components/AddJobModal.tsx
@@ -1,27 +1,56 @@
 import { useState } from "react";
+import { Methods, useFetch } from "../hooks/useFetch";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type createJobProps = {
   handleCreateJob: () => void;
+  listId: number;
 };
 
-const AddJobModal = ({ handleCreateJob }: createJobProps) => {
+const AddJobModal = ({ handleCreateJob, listId }: createJobProps) => {
   const [company, setCompany] = useState<string>("");
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [url, setUrl] = useState<string>("");
   const [salary, setSalary] = useState<string>("");
   const [location, setLocation] = useState<string>("");
+  const [attachment, setAttachment] = useState<string[]>([]);
+  const fetchData = useFetch();
+  const queryClient = useQueryClient();
 
-  const cancelAddList = (
+  const { mutate: createJob } = useMutation({
+    mutationFn: async () =>
+      await fetchData(
+        "/job/create",
+        Methods.PUT,
+        {
+          company,
+          title,
+          description,
+          url,
+          salary,
+          location,
+          list_id: listId,
+          attachment,
+        },
+        localStorage.getItem("token") ?? undefined
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["job"] });
+      handleCreateJob();
+    },
+  });
+
+  const cancelAddJob = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     event.preventDefault();
     handleCreateJob();
   };
 
-  const handleSubmitList = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmitJob = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    handleCreateJob();
+    createJob();
   };
 
   const handleInputChange = (item: string, value: string) => {
@@ -43,77 +72,75 @@ const AddJobModal = ({ handleCreateJob }: createJobProps) => {
         break;
       case "Location":
         setLocation(value);
+        break;
+      case "Attachment":
+        setAttachment([value]);
     }
   };
 
   return (
     <>
-      <div className="fixed inset-0 bg-slate-500 bg-opacity-75 transition-opacity"></div>
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-            <form
-              className="flex flex-col justify-center items-start"
-              onSubmit={handleSubmitList}
+      <div className="fixed inset-0 bg-slate-500 bg-opacity-75 transition-opacity z-10"></div>
+      <div className="fixed inset-0 z-20 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center text-center sm:items-center sm:p-0 z-30">
+          <form
+            className="bg-zinc-900 rounded-md p-10 text-white w-1/4 z-50"
+            onSubmit={handleSubmitJob}
+          >
+            {[
+              "Company",
+              "Title",
+              "Description",
+              "Url",
+              "Salary",
+              "Location",
+              "Attachment",
+            ].map((item) => {
+              return (
+                <>
+                  <div className="flex flex-col">
+                    <label key={`${item}-label`} htmlFor={item} className="">
+                      {item}
+                    </label>
+                    <input
+                      id={item}
+                      required={item === ("Company" || "Title") ? true : false}
+                      key={`${item}-input`}
+                      className="p-1 text-black rounded-md"
+                      onChange={(e) => handleInputChange(item, e.target.value)}
+                      value={
+                        item === "Company"
+                          ? company
+                          : item === "Title"
+                          ? title
+                          : item === "Description"
+                          ? description
+                          : item === "URL"
+                          ? url
+                          : item === "Salary"
+                          ? salary
+                          : item === "location"
+                          ? location
+                          : attachment
+                      }
+                    ></input>
+                  </div>
+                </>
+              );
+            })}
+            <button
+              className="bg-black px-2 rounded-md text-white align-middle text-center"
+              type="submit"
             >
-              {[
-                "Company",
-                "Title",
-                "Description",
-                "Url",
-                "Salary",
-                "Location",
-              ].map((item) => {
-                return (
-                  <>
-                    <div>
-                      <label
-                        key={`${item}-label`}
-                        htmlFor={item}
-                        className="mr-3"
-                      >
-                        {item}
-                      </label>
-                      <input
-                        required
-                        id={item}
-                        key={`${item}-input`}
-                        className="outline outline-1 outline-gray-400 px-2"
-                        onChange={(e) =>
-                          handleInputChange(item, e.target.value)
-                        }
-                        value={
-                          item === "Company"
-                            ? company
-                            : item === "Title"
-                            ? title
-                            : item === "Description"
-                            ? description
-                            : item === "URL"
-                            ? url
-                            : item === "Salary"
-                            ? salary
-                            : location
-                        }
-                      ></input>
-                    </div>
-                  </>
-                );
-              })}
-              <button
-                className="bg-black px-2 rounded-md text-white align-middle text-center"
-                type="submit"
-              >
-                Save
-              </button>
-              <button
-                className="bg-black px-2 rounded-md text-white align-middle text-center"
-                onClick={cancelAddList}
-              >
-                Cancel
-              </button>
-            </form>
-          </div>
+              Save
+            </button>
+            <button
+              className="bg-black px-2 rounded-md text-white align-middle text-center"
+              onClick={cancelAddJob}
+            >
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </>
